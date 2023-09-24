af886cc6d1bca2b02e21bff8581ed990cbc88ae1
nedagosa-cod <nestorcode4004@gmail.com>
2023-09-24 13:09:28 -0500

    end time

--- src/js/index.js
+++ src/js/index.js
@@ -30,35 +30,35 @@ elements.forEach((element) => createAnimation(element));
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 23, 2023 16:00:00").getTime();
+// var countDownDate = new Date("Sep 23, 2023 16:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+//   // Get today's date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("box-counter__dia").innerHTML = `${days}\n<span>DÍAS</span>`;
-  document.getElementById("box-counter__hora").innerHTML = `${hours}\n<span>HORAS</span>`;
-  document.getElementById("box-counter__min").innerHTML = `${minutes}\n<span>MINUTOS</span>`;
-  document.getElementById("box-counter__seg").innerHTML = `${seconds}\n<span>SEGUNDOS</span>`;
+//   // Display the result in the element with id="demo"
+//   document.getElementById("box-counter__dia").innerHTML = `${days}\n<span>DÍAS</span>`;
+//   document.getElementById("box-counter__hora").innerHTML = `${hours}\n<span>HORAS</span>`;
+//   document.getElementById("box-counter__min").innerHTML = `${minutes}\n<span>MINUTOS</span>`;
+//   document.getElementById("box-counter__seg").innerHTML = `${seconds}\n<span>SEGUNDOS</span>`;
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
 
 
 AOS.init();
